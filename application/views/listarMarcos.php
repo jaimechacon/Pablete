9588727ac1407a1d3741017172bd1ae1b9ead665
jaimechacon <jfchacona@gmail.com>
2019-10-01 15:39:48 -0300

    se pasan cambios de minsal presupuesto

--- application/views/listarMarcos.php
+++ application/views/listarMarcos.php
@@ -7,12 +7,73 @@
 	
 ?>
 <div class="row pt-3">
-	<div class="col-sm-6">
+	<div class="col-sm-12">
 		<div id="titulo" class="mt-3">
 			<h3 class="pl-3"><i class="plusTitulo mb-2" data-feather="list" ></i> Lista de Marcos
 			</h3>
 		</div>
+	<hr class="my-3">
+		<div class="row">
+			<div class="col-sm-12 p-3">	
+				<div class="row">			
+					<div class="col-sm-6">
+						<div class="row">
+							<div class="col-sm-3">
+								<span class="">Instituci&oacute;n</span>
+							</div>
+							<div class="col-sm-9">
+								<select id="institucion" class="custom-select custom-select-sm">
+								   	<option value="-1">Todos</option>
+									<?php 
+									if($instituciones)
+									{
+										foreach ($instituciones as $institucion) {
+											if(isset($idInstitucion) && (int)$institucion['id_institucion'] == $idInstitucion)
+	                                        {
+	                                                echo '<option value="'.$institucion['id_institucion'].'" selected>'.$institucion['nombre'].'</option>';
+	                                        }else
+	                                        {
+	                                                echo '<option value="'.$institucion['id_institucion'].'">'.$institucion['nombre'].'</option>';
+	                                        }
+										}
+									}
+									?>
+								</select>
+							</div>
+						</div>
+					</div>
+
+
+					<div class="col-sm-6">
+						<div class="row">
+							<div class="col-sm-3">
+								<span class="">Programas</span>
+							</div>
+							<div class="col-sm-8">
+								<input type="text" class="form-control" id="idPrograma" minlength="1" placeholder="Seleccione un Programa" name="idPrograma" value="" hidden>
+								<input type="text" class="form-control" id="inputPrograma" minlength="1" placeholder="Seleccione un Programa" name="inputPrograma" readonly>
+							</div>
+							<div class="col-sm-1">
+								<div class="row">
+									<div class="col-sm-3">
+										<button href="SeleccionarProgramaP" class="btn btn-link" type="button" id="btnBuscarPrograma"  data-toggle="modal" data-target="#modalBuscarPrograma" style="padding-top: 6px;">
+											<i stop-color data-feather="plus" class="mb-2" data-toggle="tooltip" data-placement="top" title="Seleccionar un Programa"></i>
+										</button>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				
+
+
+
+
+
+			</div>
+		</div>
 	</div>
+</div>
 	<!--<div id="filtros" class="col-sm-6">
 		<div class="mt-1 ml-1 row">
 			<label for="colFormLabelSm" class="col-sm-1 col-form-label col-form-label-sm">Buscar</label>
@@ -21,7 +82,7 @@
 			</div>
 		</div>
 	</div>-->
-	<div id="agregarCampania" class="col-sm-6 text-right">
+	<div id="agregarCampania" class="col-sm-12 pt-3 text-right">
 		<a href="asignarMarco" class="btn btn-link"><i stop-color data-feather="plus" class="pb-1"></i>Asignar Marco</a>
 	</div>
 </div>
@@ -124,3 +185,54 @@
 	    </div>
 	  </div>
 	</div>
+
+	<!-- Modal Mensaje -->
+<div class="modal fade" id="modalBuscarPrograma" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-lg" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="tituloMP">Selecciona un Programa</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      	<div class="table-responsive" id="listaSeleccionPrograma">
+			<table id="tListaProgramas" class="table table-sm table-hover table-bordered">
+				<thead class="thead-dark">
+					<tr>
+						<th scope="col" class="texto-pequenio text-center align-middle registro"># ID</th>
+					    <th scope="col" class="texto-pequenio text-center align-middle registro">Nombre</th>
+					    <th scope="col" class="texto-pequenio text-center align-middle registro">Descripci&oacute;n</th>
+					    <th scope="col" class="texto-pequenio text-center align-middle registro">Forma de Pago</th>
+					    	<th scope="col" class="texto-pequenio text-center align-middle registro"></th>
+					</tr>
+				</thead>
+				<tbody id="tbodyPrograma">
+			        <?php
+			        if(isset($programas))
+			        {
+				        foreach ($programas as $programa): ?>
+				  			<tr>
+						        <th scope="row" class="text-center align-middle registro"><p class="texto-pequenio"><?php echo $programa['id_programa']; ?></th>
+						        <td class="text-center align-middle registro"><p class="texto-pequenio"><?php echo $programa['nombre']; ?></p></td>
+						        <td class="text-center align-middle registro"><p class="texto-pequenio"><?php echo $programa['descripcion']; ?></p></td>
+						        <td class="text-center align-middle registro"><p class="texto-pequenio"><?php echo $programa['forma_pago']; ?></p></td>
+						        <td class="text-center align-middle registro botonTabla paginate_button">
+					        		<button href="#" aria-controls="tListaProgramas" data-id="<?php echo $programa['id_programa']; ?>" data-nombre="<?php echo $programa['nombre']; ?>" tabindex="0" class="btn btn-outline-dark seleccionPrograma">Seleccionar</button>
+					        	</td>
+					    	</tr>
+				  		<?php endforeach;
+			  		}?>
+			  </tbody>
+			</table>
+		</div>
+      </div>
+      <div class="modal-footer">
+        <button id="btnCerrarMP" type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div id="loader" class="loader" hidden></div>
