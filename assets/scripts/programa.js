35aa1cc20beb8d04698fafe4b76cc681ab6b39ae
jaimechacon <jfchacona@gmail.com>
2019-10-01 15:45:53 -0300

    se pasan cambios de minsal presupuesto

--- assets/scripts/programa.js
+++ assets/scripts/programa.js
@@ -101,6 +101,34 @@
 
   });
 
+  $('#tListaProgramas').dataTable({
+        searching: true,
+        paging:         true,
+        ordering:       true,
+        info:           true,
+        columnDefs: [
+          { targets: 'no-sort', orderable: false }
+        ],
+        //bDestroy:       true,
+         
+        "oLanguage": {
+            "sLengthMenu": "_MENU_ Registros por p&aacute;gina",
+            "sZeroRecords": "No se encontraron registros",
+            "sInfo": "Mostrando del _START_ al _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrando 0 de 0 registros",
+            "sInfoFiltered": "(filtrado de _MAX_ registros totales)",
+            "sSearch":        "Buscar:",
+            "sProcessing" : '<img src="<?php echo base_url(); ?>images/gif/spin2.svg" height="42" width="42" >',
+            "oPaginate": {
+                "sFirst":    "Primero",
+                "sLast":    "Último",
+                "sNext":    "Siguiente",
+                "sPrevious": "Anterior"
+            }
+        },
+        lengthMenu: [[10, 20], [10, 20]]
+    });
+
  $('#idInstitucionP').on('changed.bs.select', function (e, clickedIndex, isSelected, previousValue) {
     obtenerFiltrosTransferencias(1);
   });
